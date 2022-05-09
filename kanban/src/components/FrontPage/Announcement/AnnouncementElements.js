2e5445f9c5a4c597b1fff0df584c8768522488b4
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-09 21:04:17 +0200

    Announcement

---
+++ kanban/src/components/FrontPage/Announcement/AnnouncementElements.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 30px;
+  background-color: #562bf6;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.8rem;
+  font-weight: 500;
+`;
+export const Text = styled.div``
