30b6d42795552dae5279e065a06dd6b296979eeb
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-10 10:54:51 +0200

    learn more

---
+++ kanban/src/components/FrontPage/LearnMore/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from "./LearnMoreElements"
+
+const LearnMore = () => {
+  return (
+    <Container>
+        Learn more page
+    </Container>
+  )
+};
+
+export default LearnMore;
