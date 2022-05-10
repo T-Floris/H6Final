08f8a1c262114b4005fa9e507d648789b84c6b6c
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-10 10:53:18 +0200

    added i18next

--- kanban/src/index.js
+++ kanban/src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "da", "zh"],
+    fallbackLng: "en",
+    detection: {
+      // order and from where user language should be detected
+      order: ["path", "cookie", "htmlTag", "localStorage", "subdomain"],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/Locales/{{lng}}/translation.json",
+    },
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
