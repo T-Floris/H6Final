93205f24c1b129c463991b4b6689eb1739f4ece6
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-11 11:10:19 +0200

    added flag icon & package

--- kanban/src/index.js
+++ kanban/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
+import "flag-icon-css/css/flag-icon.min.css";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
