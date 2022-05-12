fc8ad604c89a6a55274ac95a04c5d3d561e426b5
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-12 10:35:43 +0200

    terms

---
+++ kanban/src/components/FrontPage/TermsAndConditions/TermsAndConditionsElements.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #fff;
+`;
+export const InfoContainer = styled.div`
+  background-color: #fff;
+  margin: 5% 10% 10% 10%;
+`;
+export const TitleTop = styled.h1`
+  text-align: center;
+  margin-bottom: 50px;
+`;
+export const TitleH2 = styled.h2``;
+export const Date = styled.p`
+  font-weight: bold;
+`;
+export const Text = styled.p`
+  margin: 15px 0;
+`;
+export const UL = styled.ul``;
+export const LI = styled.li`
+  margin: 15px 0px 15px 50px;
+  @media only screen and (max-width: 380px) {
+    margin: 15px 0px 15px 20px;
+  }
+`;
