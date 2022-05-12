b5dd40094aaa77148b1a975aff2635459e677141
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-12 10:54:50 +0200

    adjustment

--- kanban/src/components/FrontPage/Announcement/index.js
+++ kanban/src/components/FrontPage/Announcement/index.js
@@ -3,8 +3,10 @@ import { useTranslation } from "react-i18next";
 import { Container, Text } from "./AnnouncementElements";
 
 const Announcement = () => {
+
   //i18next
   const { t } = useTranslation();
+  
   return (
     <Container>
       <Text>{t("announcement_text")}</Text>
