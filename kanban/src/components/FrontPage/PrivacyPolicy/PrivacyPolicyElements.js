9f27fadbf51ae5c4977a61c60f511336b23cf988
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-12 10:35:33 +0200

    privacy

---
+++ kanban/src/components/FrontPage/PrivacyPolicy/PrivacyPolicyElements.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #fff;
+`;
+export const InfoContainer = styled.div`
+  background-color: #fff;
+  margin: 5% 10% 10% 10%;
+`;
+export const TitleTop = styled.h1`
+  text-align: center;
+  margin-bottom: 50px;
+`;
+export const TitleH1 = styled.h2`
+  margin-bottom: 30px;
+`;
+export const TitleH2 = styled.h3`
+  margin-bottom: 30px;
+`;
+export const TitleH3 = styled.h4`
+  margin-bottom: 30px;
+`;
+export const Date = styled.p`
+  font-weight: bold;
+`;
+
+export const Text = styled.p`
+  margin: 30px 0;
+`;
+export const TextToRight = styled.p`
+  margin: 15px 0px 15px 80px;
+  @media only screen and (max-width: 380px) {
+    margin: 15px 0px 15px 20px;
+  }
+`;
+export const UL = styled.ul``;
+export const LI = styled.li`
+  margin: 15px 0px 15px 50px;
+  @media only screen and (max-width: 380px) {
+    margin: 15px 0px 15px 20px;
+  }
+`;
