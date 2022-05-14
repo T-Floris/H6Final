9a590efc754338d9de3f3e9415c26b1f25e676d9
Tino Floris <tino0100@elev.tec.dk>
2022-05-14 15:47:15 +0200

    Create TokenLifespanConfiguration.cs

---
+++ visual Studio/Kanban/KanbanApi/Configuration/TokenLifespan/TokenLifespanConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace KanbanApi.Configuration.TokenLifespan
+{
+    public class TokenLifespanConfiguration
+    {
+        public int DataProtection { get; set; }
+        public int Authenticator { get; set; }
+        public int ChangeEmail { get; set; }
+        public int ChangePhoneNumber { get; set; }
+        public int PasswordReset { get; set; }
+        public int EmailConfirmation { get; set; }
+    }
+}
