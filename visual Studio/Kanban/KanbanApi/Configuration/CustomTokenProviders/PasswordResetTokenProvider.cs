7d43958adf5fb88712c7e23db76de1907f24dab3
Tino Floris <tino0100@elev.tec.dk>
2022-05-14 15:47:02 +0200

    token providers

---
+++ visual Studio/Kanban/KanbanApi/Configuration/CustomTokenProviders/PasswordResetTokenProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace KanbanApi.Configuration.CustomTokenProviders
+{
+    public class PasswordResetTokenProvider<TUser> : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public PasswordResetTokenProvider(IDataProtectionProvider dataProtectionProvider,
+            IOptions<PasswordResetTokenProviderOptions> options,
+            ILogger<DataProtectorTokenProvider<TUser>> logger)
+            : base(dataProtectionProvider, options, logger)
+        {
+        }
+    }
+
+    public class PasswordResetTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+
+    }
+}
