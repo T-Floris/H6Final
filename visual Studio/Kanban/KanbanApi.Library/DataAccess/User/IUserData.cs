51aed0ffd2c3a20a2af2ad49cfb7e626c529dd41
Tino Floris <tino0100@elev.tec.dk>
2022-05-14 13:36:25 +0200

    class and interface for "UserData"

---
+++ visual Studio/Kanban/KanbanApi.Library/DataAccess/User/IUserData.cs
@@ -0,0 +1,24 @@
+﻿using KanbanApi.Library.DTOs.Requests.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanApi.Library.DataAccess.User
+{
+    public interface IUserData
+    {
+        void Registration(RegistrationRequest registration);
+
+        void UpdateEmail(UpdateEmailRequest updateEmail);
+        void UpdateUserName(UpdateUserNameRequest updateUserName);
+
+        void DeleteUser(DeleteUserRequest deleteUser);
+
+        //UserModel GetUserById(string Id);
+
+
+
+    }
+}
---
+++ visual Studio/Kanban/KanbanApi.Library/DataAccess/User/UserData.cs
@@ -0,0 +1,40 @@
+﻿using KanbanApi.Library.DTOs.Requests.Auth;
+using KanbanApi.Library.Internal.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanApi.Library.DataAccess.User
+{
+    public class UserData : IUserData
+    {
+        private readonly ISqlDataAccess _sql;
+
+        public UserData(ISqlDataAccess sql)
+        {
+            _sql = sql;
+        }
+
+        public void DeleteUser(DeleteUserRequest deleteUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Registration(RegistrationRequest registration)
+        {
+            _sql.SaveData("spUser_Registration", new { registration.Id, registration.FirstName, registration.LastName, registration.EmailAddress, registration.UserName, registration.Avatar}, "KanbanData");
+        }
+
+        public void UpdateEmail(UpdateEmailRequest updateEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUserName(UpdateUserNameRequest updateUserName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
