ea5d59090ec95d1b47e58551bdca7b5096903fca
Tino Floris <tino0100@elev.tec.dk>
2022-05-15 12:15:59 +0200

    Update User.sql

--- visual Studio/Kanban/KanbanData/dbo/Tables/User.sql
+++ visual Studio/Kanban/KanbanData/dbo/Tables/User.sql
@@ -4,7 +4,7 @@
 	[FirstName] nvarchar(50) NOT null,
 	[LastName] nvarchar(50) NOT null,
 	[EmailAddress] nvarchar(50) NOT NULL,
-	[UserName] nvarchar(50) NOT NULL,
+	[UserName] nvarchar(256) NOT NULL,
 	[Avatar] nvarchar(50) NOT NULL,
 	[CreatedDate] datetime2(7) NOT NULL,
 	CONSTRAINT [PK_User] PRIMARY KEY ([Id])
