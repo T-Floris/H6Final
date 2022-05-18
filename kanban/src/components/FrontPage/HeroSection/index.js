a49520bd277cf428ec341338f578444f8a7d1f63
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-18 13:12:28 +0200

    adjustments and more readable

--- kanban/src/components/FrontPage/HeroSection/index.js
+++ kanban/src/components/FrontPage/HeroSection/index.js
@@ -4,13 +4,14 @@ import { useTranslation } from "react-i18next";
 import Video from "../../../videos/video.mp4";
 import { ButtonTransparent } from "../../Assets/ButtonElements";
 import {
-  HeroContainer,
-  HeroBackground,
+  Container,
+  Background,
   VideoBackground,
-  HeroContent,
-  HeroH1,
-  HeroP,
-  HeroBtnWrapper,
+  Content,
+  Title,
+  Text,
+  BtnWrap,
+  IconWrap,
   ArrowForward,
   ArrowRight,
 } from "./HeroElements";
@@ -26,27 +27,26 @@ const HeroSection = () => {
   const { t } = useTranslation();
 
   return (
-    <HeroContainer id="home">
-      <HeroBackground>
+    <Container id="home">
+      <Background>
         <VideoBackground autoPlay loop muted src={Video} type="video/mp4" />
-      </HeroBackground>
-      <HeroContent>
-        <HeroH1>{t("kanban")}</HeroH1>
-        <HeroP>{t("hero_section_text")}</HeroP>
-        <HeroBtnWrapper>
+      </Background>
+      <Content>
+        <Title>{t("kanban")}</Title>
+        <Text>{t("hero_section_text")}</Text>
+        <BtnWrap>
           {/* Button is import from ButtonElements.js*/}
           <ButtonTransparent
             to="/signin"
             onMouseEnter={onHover}
             onMouseLeave={onHover}
-            primary="true"
-            dark="true"
           >
-            {t("hero_section_button_text")} {hover ? <ArrowForward /> : <ArrowRight />}
+            {t("hero_section_button_text")}{" "}
+            <IconWrap>{hover ? <ArrowRight /> : <ArrowForward />}</IconWrap>
           </ButtonTransparent>
-        </HeroBtnWrapper>
-      </HeroContent>
-    </HeroContainer>
+        </BtnWrap>
+      </Content>
+    </Container>
   );
 };
 
