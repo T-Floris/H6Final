3683c1f312323897bc2cace1da58cf86c582e688
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-23 11:14:53 +0200

    Added welcome message

--- kanban/src/components/UserInterface/UserStart/index.js
+++ kanban/src/components/UserInterface/UserStart/index.js
@@ -3,13 +3,27 @@ import { Container, Title, Text, Wrapper } from "./StartElements";
 import { PageBar } from "../../Layout/CurrentPageBar/CurrentPageBarElements";
 
 const Home = () => {
+
+  //Welcome message for user
+  const date = new Date();
+  const hours = date.getHours();
+  let message;
+
+  if (hours < 12) {
+    message = "Good Morning";
+  } else if (hours < 18) {
+    message = "Good Afternoon";
+  } else {
+    message = "Good Evening";
+  }
+  
   return (
     <Container>
       <PageBar>
         <Title>Home Page</Title>
       </PageBar>
       <Wrapper>
-        <Text>Something...</Text>
+        <Text>{message} User</Text>
       </Wrapper>
     </Container>
   );
