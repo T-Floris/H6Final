e443a05ae1a32c2b6731f4674bac09e956fbbcd6
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-23 11:05:38 +0200

    Routes update

--- kanban/src/NavigateRoutes.js
+++ kanban/src/NavigateRoutes.js
@@ -7,9 +7,15 @@ import PrivacyPolicyPage from "./pages/FrontPages/PrivacyPolicyPage";
 import TermsAndConditionsPage from "./pages/FrontPages/TermsAndConditionsPage";
 import ContactFormPage from "./pages/FrontPages/ContactFormPage";
 import LearnMorePage from "./pages/FrontPages/LearnMorePage";
+//Auth pages
+
+//User pages
+import UserStartPage from "./pages/UserPages/UserStartPage";
+
+//Admin pages
+
 // 404
 import NotFoundPage from "./pages/404/NotFoundPage";
-
 
 const NavigateRoutes = () => {
   return (
@@ -23,6 +29,14 @@ const NavigateRoutes = () => {
         <Route path="/terms" element={<TermsAndConditionsPage />} />
         <Route path="/learnmore" element={<LearnMorePage />} />
 
+        {/* Auth pages */}
+        
+
+        {/* User pages */}
+        <Route path="/userstart" element={<UserStartPage />} />
+
+
+        {/* Admin pages */}
 
         {/* 404 */}
         <Route path="*" element={<NotFoundPage />} />
