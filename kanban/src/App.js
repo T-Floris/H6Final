5ead6e722cfaa807bf24f6499ad326000c2e85f2
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-23 11:05:04 +0200

    Added theme for dark mode

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -1,11 +1,25 @@
+import React, { useState } from "react";
+
 import { GlobalStyle } from "./components/Assets/GlobalStyles";
 import NavigateRoutes from "./NavigateRoutes";
+import { darkTheme, lightTheme } from "../src/components/Assets/theme";
+import { ThemeProvider } from "styled-components";
+
+export const ThemeContext = React.createContext(null);
 
 function App() {
+  //Theme
+  const [theme, setTheme] = useState("light");
+  const themeStyle = theme === "light" ? lightTheme : darkTheme;
+
   return (
     <>
-      <GlobalStyle />
-      <NavigateRoutes />
+      <ThemeContext.Provider value={{ setTheme, theme }}>
+        <ThemeProvider theme={themeStyle}>
+          <GlobalStyle />
+          <NavigateRoutes />
+        </ThemeProvider>
+      </ThemeContext.Provider>
     </>
   );
 }
