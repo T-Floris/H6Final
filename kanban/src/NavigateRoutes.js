c0da5838d9d32d0434d3caad211cab0a5c6a55c9
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-24 23:02:28 +0200

    routes setup

--- kanban/src/NavigateRoutes.js
+++ kanban/src/NavigateRoutes.js
@@ -11,11 +11,20 @@ import LearnMorePage from "./pages/FrontPages/LearnMorePage";
 
 //User pages
 import UserStartPage from "./pages/UserPages/UserStartPage";
+import WorkspacePage from "./pages/UserPages/WorkspacePage";
+import BoardPage from "./pages/UserPages/BoardsPage";
+import GroupPage from "./pages/UserPages/GroupPage";
+import CalendarPage from "./pages/UserPages/CalendarPage";
+import MessagePage from "./pages/UserPages/MessagePage";
+import SettingsPage from "./pages/UserPages/SettingsPage";
+import UserProfilePage from "./pages/UserPages/UserProfilePage";
 
 //Admin pages
+import AdminDashboardPage from "./pages/Admin/AdminDashboardPage"
 
 // 404
 import NotFoundPage from "./pages/404/NotFoundPage";
+
 
 const NavigateRoutes = () => {
   return (
@@ -34,10 +43,17 @@ const NavigateRoutes = () => {
 
         {/* User pages */}
         <Route path="/userstart" element={<UserStartPage />} />
-
+        <Route path="/workspace" element={<WorkspacePage />} />
+        <Route path="/board" element={<BoardPage />} />
+        <Route path="/group" element={<GroupPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/message" element={<MessagePage />} />
+        <Route path="/userprofile" element={<UserProfilePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
 
         {/* Admin pages */}
-
+        <Route path="/admin" element={<AdminDashboardPage />} />
+        
         {/* 404 */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
