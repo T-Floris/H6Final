8e30e79bd2793f52f42dbc38bd09ca93167dacdd
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-24 23:03:00 +0200

    admin setup

---
+++ kanban/src/components/Admin/AdminDashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Container} from "./AdminDashboardElements"
+
+const AdminDashboard = () => {
+  return (
+    <Container>AdminDashboard</Container>
+  )
+}
+
+export default AdminDashboard
---
+++ kanban/src/pages/Admin/AdminDashboardPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import AdminDashboard from "../../components/Admin/AdminDashboard";
+import { backgroundTheme } from "../../components/Assets/variables";
+import MetaDecorator from "../../components/Utils/Meta/MetaDecorator";
+const MetaData = require("../../components/Utils/Meta/MetaData.json");
+
+const Container = styled.div`
+  ${backgroundTheme}
+`;
+
+const AdminDashboardPage = () => {
+  return (
+    <Container>
+      <MetaDecorator
+        title={MetaData.admindashboardPageTitle}
+        description={MetaData.admindashboardPageDesc}
+      />
+      
+      <AdminDashboard />
+    </Container>
+  );
+};
+
+export default AdminDashboardPage;
