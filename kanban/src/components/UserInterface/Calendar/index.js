ef29f454bc3891d315d04728210ed82d4c7a11e4
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-25 08:50:22 +0200

    Calendar setup

---
+++ kanban/src/components/UserInterface/Calendar/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Title, Text, Wrapper } from "./CalendarElements";
+import { PageBar } from "../../Layout/CurrentPageBar/CurrentPageBarElements";
+
+const Calendar = () => {
+  return (
+    <Container>
+      <PageBar>
+        <Title>Calendar</Title>
+      </PageBar>
+      <Wrapper>
+        <Text>Something...</Text>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Calendar;
---
+++ kanban/src/pages/UserPages/CalendarPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { backgroundTheme } from "../../components/Assets/variables";
+import Sidebar from "../../components/Layout/Sidebar";
+import Calendar from "../../components/UserInterface/Calendar"
+import MetaDecorator from "../../components/Utils/Meta/MetaDecorator";
+const MetaData = require("../../components/Utils/Meta/MetaData.json");
+
+const Container = styled.div`
+  ${backgroundTheme}
+`;
+
+const CalendarPage = () => {
+  return (
+    <Container>
+      <MetaDecorator
+        title={MetaData.calendarPageTitle}
+        description={MetaData.calendarPageDesc}
+      />
+      <Sidebar />
+      <Calendar />
+    </Container>
+  );
+};
+
+export default CalendarPage;
