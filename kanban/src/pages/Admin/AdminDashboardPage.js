226742058cfb6cf2ab50a4d9d02579dc71a2889d
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-25 11:00:36 +0200

    adjustments

--- kanban/src/pages/Admin/AdminDashboardPage.js
+++ kanban/src/pages/Admin/AdminDashboardPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import AdminDashboard from "../../components/Admin/AdminDashboard";
+import AdminDashboard from "../../components/Admin";
 import { backgroundTheme } from "../../components/Assets/variables";
 import MetaDecorator from "../../components/Utils/Meta/MetaDecorator";
 const MetaData = require("../../components/Utils/Meta/MetaData.json");
