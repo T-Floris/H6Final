6b3b92a88f12674d5017e1b48f145170785a589b
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-05-25 08:54:02 +0200

    board setup

---
+++ kanban/src/pages/UserPages/BoardsPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import UserBoard from "../../components/UserInterface/UserBoard"
+import styled from "styled-components";
+import MetaDecorator from "../../components/Utils/Meta/MetaDecorator";
+import Sidebar from "../../components/Layout/Sidebar";
+import { backgroundTheme } from "../../components/Assets/variables";
+const MetaData = require("../../components/Utils/Meta/MetaData.json");
+
+const BoardContainer = styled.div`
+  ${backgroundTheme}
+`;
+
+const BoardPage = () => {
+  return (
+    <BoardContainer>
+      <MetaDecorator
+        title={MetaData.boardPageTitle}
+        description={MetaData.boardPageDesc}
+      />
+
+      <Sidebar />
+      <UserBoard />
+    </BoardContainer>
+  );
+};
+
+export default BoardPage;
