aec15610447488d2f9be0f638046a6f292aabd7c
Tino Floris <tino0100@elev.tec.dk>
2022-05-29 12:56:19 +0200

    create table

--- visual Studio/Kanban/KanbanApi.Library/DTOs/Results/Table/CreateTableResult.cs
+++ visual Studio/Kanban/KanbanApi.Library/DTOs/Results/Table/CreateTableResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KanbanApi.Library.DTOs.Responses.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace KanbanApi.Library.DTOs.Results.Table
 {
-    public class CreateTableResult
+    public class CreateTableResult : CreateTableResponse
     {
+        public List<string> Message { get; set; }
     }
 }
