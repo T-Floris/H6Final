67e6c6194710bb85e0412d2baff79b1cc8f02fbe
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-06-04 14:33:48 +0200

    comments

--- kanban/src/api/axios.js
+++ kanban/src/api/axios.js
@@ -4,9 +4,9 @@ const BASE_URL = 'https://localhost:44338/api/';
 export default axios.create({
     baseURL: BASE_URL
 });
-
+//interceptors and attach the jwt token for us and try when we fail which comes back as 403 forbidden
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+  withCredentials: true,//this setting allows us to send cookies with our request and send along our cookie that has the response token
 });
