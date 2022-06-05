c5b54e28bba53939157ae1ef23156e219b2ac3e0
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-06-05 20:46:57 +0200

    routes update

--- kanban/src/NavigateRoutes.js
+++ kanban/src/NavigateRoutes.js
@@ -10,9 +10,10 @@ import LearnMorePage from "./pages/FrontPages/LearnMorePage";
 //Auth pages
 import RegisterPage from "./pages/AuthPages/User/RegisterPage";
 import LoginPage from "./pages/AuthPages/User/LoginPage";
+import AdminLoginPage from "./pages/AuthPages/Admin/AdminLoginPage";
 import RequireAuth from "./components/Auth/RequireAuth";
 import Unauthorized from "./components/Auth/Unauthorized";
-import AdminLoginPage from "./pages/AuthPages/Admin/AdminLoginPage";
+import PersistLogin from "./components/Auth/PersistLogin";
 //User pages
 import UserStartPage from "./pages/UserPages/UserStartPage";
 import WorkspacePage from "./pages/UserPages/WorkspacePage";
@@ -22,7 +23,7 @@ import CalendarPage from "./pages/UserPages/CalendarPage";
 import MessagePage from "./pages/UserPages/MessagePage";
 import SettingsPage from "./pages/UserPages/SettingsPage";
 import UserProfilePage from "./pages/UserPages/UserProfilePage";
-import TemplateBoardPage from "./pages/UserPages/Template/TemplateBoardPage"
+import TemplateBoardPage from "./pages/UserPages/Template/TemplateBoardPage";
 
 //Admin pages
 import DashboardPage from "./pages/AdminPages/DashboardPage";
@@ -32,19 +33,18 @@ import UserListPage from "./pages/AdminPages/UserListPage";
 import Layout from "./components/Layout";
 
 //Email pages
-import ChangeEmailPage from "./pages/EmailPages/ChangeEmailPage"
-import ConfirmEmailPage from "./pages/EmailPages/ConfirmEmailPage"
-import ForgotPasswordPage from "./pages/EmailPages/ForgotPasswordPage"
+import ChangeEmailPage from "./pages/EmailPages/ChangeEmailPage";
+import ConfirmEmailPage from "./pages/EmailPages/ConfirmEmailPage";
+import ForgotPasswordPage from "./pages/EmailPages/ForgotPasswordPage";
 
 // 404
 import NotFoundPage from "./pages/404Pages/NotFoundPage";
 
 const ROLES = {
-  'User': "User",
-  'Admin': "Admin",
-  'Superuser' : "Superuser"
-}
-
+  User: "User",
+  Admin: "Admin",
+  Superuser: "Superuser",
+};
 
 const NavigateRoutes = () => {
   return (
@@ -77,24 +77,26 @@ const NavigateRoutes = () => {
 
           {/* Protected routes */}
           {/* User pages */}
-          <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-            {" "}
-            {/* redirect to login page if not login */}
-            <Route path="/userstart" element={<UserStartPage />} />
-            <Route path="/workspace" element={<WorkspacePage />} />
-            <Route path="/board" element={<BoardPage />} />
-            <Route path="/templateboard" element={<TemplateBoardPage />} />
-            <Route path="/group" element={<GroupPage />} />
-            <Route path="/calendar" element={<CalendarPage />} />
-            <Route path="/message" element={<MessagePage />} />
-            <Route path="/userprofile" element={<UserProfilePage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-          </Route>
+          <Route element={<PersistLogin />}>
+            <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+              {" "}
+              {/* redirect to login page if not login */}
+              <Route path="/userstart" element={<UserStartPage />} />
+              <Route path="/workspace" element={<WorkspacePage />} />
+              <Route path="/board" element={<BoardPage />} />
+              <Route path="/templateboard" element={<TemplateBoardPage />} />
+              <Route path="/group" element={<GroupPage />} />
+              <Route path="/calendar" element={<CalendarPage />} />
+              <Route path="/message" element={<MessagePage />} />
+              <Route path="/userprofile" element={<UserProfilePage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+            </Route>
 
-          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
-            {/* Admin pages */}
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/userlist" element={<UserListPage />} />
+            <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+              {/* Admin pages */}
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/userlist" element={<UserListPage />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
