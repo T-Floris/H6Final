98f7adf791b92c86890d97740a1cda612b31a043
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-06-05 13:47:43 +0200

    added get users username

--- kanban/src/components/UserInterface/UserStart/index.js
+++ kanban/src/components/UserInterface/UserStart/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Container, Title, Text, Wrapper } from "./StartElements";
 import { PageBar } from "../../Menus/CurrentPageBar/CurrentPageBarElements";
 import { useTranslation } from "react-i18next";
+import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Home = () => {
-
-//i18next
-const { t } = useTranslation();
+  //i18next
+  const { t } = useTranslation();
 
   //Welcome message for user
   const date = new Date();
@@ -20,14 +23,50 @@ const { t } = useTranslation();
   } else {
     message = t("userstart_evening");
   }
-  
+
+  const [user, setUser] = useState({
+    userName: "",
+    emailAddress: "",
+    firstName: "",
+    lastName: "",
+    id: "",
+  });
+  // const navigate = useNavigate();
+  // const location = useLocation();
+  const axiosPrivate = useAxiosPrivate();
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUser = async () => {
+      try {
+        const response = await axiosPrivate.get("user", {
+          signal: controller.signal,
+        });
+        console.log(response.data);
+        isMounted && setUser(response.data);
+      } catch (err) {
+        // console.error(err);
+        // navigate("/login", { state: { from: location }, replace: true });
+      }
+    };
+
+    getUser();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
   return (
     <Container>
       <PageBar>
         <Title>Home Page</Title>
       </PageBar>
       <Wrapper>
-        <Text>{message} User</Text>
+        <Text>{message} {user.userName}</Text>
+
       </Wrapper>
     </Container>
   );
